f8fa66d74a7c893e2b1cbbb341c6051324be3215
santosh-1107 <saisantoshpreetham@gmail.com>
2026-02-25 14:23:12 +0530

    uc9:Implemented recursive palindrome checker

--- src/Main.java
+++ src/Main.java
@@ -2,62 +2,24 @@ import java.util.*;
 
 class PalindromeCheckerApp {
 
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
     public static void main(String[] args) {
-        String input = "level";
-        Node head = null;
-        Node tail = null;
 
-        for (char c : input.toCharArray()) {
-            Node newNode = new Node(c);
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-        boolean isPalindrome = isPalindrome(head);
+        String input = "madam";
+
+        boolean isPalindrome = check(input, 0, input.length() - 1);
+
+        // Display result
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
-    public static boolean isPalindrome(Node head) {
 
-        if (head == null || head.next == null)
+    private static boolean check(String s, int start, int end) {
+        if (start >= end) {
             return true;
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
         }
-        Node prev = null;
-        Node current = slow;
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
         }
-        Node firstHalf = head;
-        Node secondHalf = prev;
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-        return true;
+        return check(s, start + 1, end - 1);
     }
 }
